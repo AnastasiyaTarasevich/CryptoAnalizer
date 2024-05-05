34e35f5b59716cfeba82d6d86034cd6754bfe607
Anastasiya Tarasevich <at9183002@gmail.com>
2024-05-05 18:44:16 +0300

    complete Decode execute function and add Reading class

--- services/Decode.java
+++ services/Decode.java
@@ -2,18 +2,51 @@ package com.javarush.task.jdk13.task53.task5307.services;
 
 import com.javarush.task.jdk13.task53.task5307.Entity.Result;
 import com.javarush.task.jdk13.task53.task5307.Exception.ApplicationException;
+import com.javarush.task.jdk13.task53.task5307.constants.CryptoAlphabet;
 import com.javarush.task.jdk13.task53.task5307.repository.ResultCode;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Decode implements Function{
+    private String defaultPathForRead="C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\encrypt.txt";
+
+    private String defaultPathForWrite="C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\decrypt.txt";
     @Override
     public Result execute(String[] parameters) {
-        try
-        {
-            //TODO finish DECODE
-        }catch (Exception e)
-        {
-            return new Result(ResultCode.ERROR,new ApplicationException("Decode operation finished with exception: ",e));
+
+        String filePath = Reading.getFilePath(defaultPathForRead);
+        int number = Reading.getKey();
+        try {
+            String text = Reading.readFile(filePath);
+            System.out.println("Зашифрованный текст: " + text);
+            String decryptedText = decrypt(text, number);
+            System.out.println("Расшифрованный текст: " + decryptedText);
+            String fileForDecrypt=Reading.getFilePath(defaultPathForWrite);
+            Reading.writeToFile(fileForDecrypt, decryptedText);
+        } catch (IOException e) {
+            throw new ApplicationException("Файла не существует", e);
         }
-        return new Result(ResultCode.OK);
+        return new Result(ResultCode.OK, "Encode");
+    }
+
+
+    private String decrypt(String text,int key)
+    {
+        StringBuilder decrypted=new StringBuilder();
+        for(char c:text.toCharArray())
+        {
+            int index= CryptoAlphabet.ALPHABET.indexOf(c);
+            if(index!=-1)
+            {
+                index=(index-key+CryptoAlphabet.ALPHABET.length())%CryptoAlphabet.ALPHABET.length();
+
+                decrypted.append(CryptoAlphabet.ALPHABET.charAt(index));
+            }
+            else {
+                decrypted.append(c);
+            }
+        }
+        return decrypted.toString();
     }
 }
--- services/Encode.java
+++ services/Encode.java
@@ -6,89 +6,29 @@ import com.javarush.task.jdk13.task53.task5307.constants.CryptoAlphabet;
 import com.javarush.task.jdk13.task53.task5307.repository.ResultCode;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Encode implements Function {
+    private String defaultPathForRead="C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\text.txt";
+
+    private String defaultPathForWrite="C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\encrypt.txt";
     @Override
     public Result execute(String[] parameters) {
 
-        String filePath = getFilePath();
-        int number = getKey();
+        String filePath = Reading.getFilePath(defaultPathForRead);
+        int number = Reading.getKey();
         try {
-            String text = readFile(filePath);
+            String text = Reading.readFile(filePath);
             System.out.println("Незашифрованный текст: " + text);
             String encryptedText = encrypt(text, number);
             System.out.println("Зашифрованный текст: " + encryptedText);
-            String fileForEncrypt=getFilePathForEncrypt();
-            writeToFile(fileForEncrypt, encryptedText);
+            String fileForEncrypt=Reading.getFilePath(defaultPathForWrite);
+            Reading.writeToFile(fileForEncrypt, encryptedText);
         } catch (IOException e) {
             throw new ApplicationException("Файла не существует", e);
         }
         return new Result(ResultCode.OK, "Encode");
     }
-
-    private static String readFile(String filePath) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-        }
-        return sb.toString();
-    }
-
-    private static void writeToFile(String path, String text)
-    {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
-            writer.write(text);
-            System.out.println("Текст успешно записан в файл: " + path);
-        } catch (IOException e) {
-            throw new ApplicationException("Ошибка записи в файл", e);
-        }
-    }
-
-    private String getFilePath() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите путь к файлу (или нажмите Enter для использования пути по умолчанию 'myFiles/text.txt'): ");
-        String filePath = scanner.nextLine();
-
-        if (filePath.isEmpty()) {
-            filePath = "C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\text.txt";
-        }
-
-        return filePath;
-    }
-    private String getFilePathForEncrypt() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите путь к файлу для записи (или нажмите Enter для использования пути по умолчанию 'myFiles/encrypt.txt'): ");
-        String filePath = scanner.nextLine();
-
-        if (filePath.isEmpty()) {
-            filePath = "C:\\Users\\Nastya\\javarush\\3415430\\javarush-project\\src\\com\\javarush\\task\\jdk13\\task53\\task5307\\myFiles\\encrypt.txt";
-        }
-
-        return filePath;
-    }
-
-    private int getKey() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите ключ для кода (или нажмите Enter для использования по умолчанию 13): ");
-        String input = scanner.nextLine();
-
-        int number;
-        if (input.isEmpty()) {
-            number = 13;
-        } else {
-            number = Integer.parseInt(input);
-        }
-        return number;
-    }
-
 
     private String encrypt(String text,int key)
     {
