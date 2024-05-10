3c1d8096df50b3f377cde9cefd2a5319df99421b
Anastasiya Tarasevich <at9183002@gmail.com>
2024-05-10 22:35:15 +0300

    fix changes_2

--- view/View.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.task.jdk13.task53.task5307.view;
-
-import com.javarush.task.jdk13.task53.task5307.Entity.Result;
-
-public interface View {
-    String [] getParameters();
-    void printResults(Result result);
-}
